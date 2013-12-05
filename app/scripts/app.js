263cc6ee7786571fd76d58911b149664771c9cf6
Jan Pochyla <jpochyla@gmail.com>
2013-12-05 16:36:58 +0100

    Delay app load until the trezor plugin is loaded

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,3 +16,16 @@ angular.module('webwalletApp', [
         redirectTo: '/'
       });
   });
+
+angular.element(document).ready(function () {
+  trezor.load(
+    function (tzr) {
+      angular.module('webwalletApp').value('trezor', tzr);
+      angular.bootstrap(document, ['webwalletApp']);
+    },
+    function (err, install) {
+      if (install) install();
+    },
+    { configUrl: 'http://127.0.0.1:9000/config_signed.bin' }
+  );
+});
