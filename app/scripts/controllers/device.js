b62cc784483263df9a852c109d6b3a8842b60e65
Jan Pochyla <jpochyla@gmail.com>
2014-01-20 18:55:11 +0100

    Controller actions for device load/setup

--- app/scripts/controllers/device.js
+++ app/scripts/controllers/device.js
@@ -3,13 +3,37 @@
 angular.module('webwalletApp')
   .controller('DeviceCtrl', function (trezorService, $scope, $location, $routeParams) {
     $scope.device = trezorService.get($routeParams.deviceId);
-    if (!$scope.device) {
-      $location.path('/');
-      return;
-    }
+    if (!$scope.device)
+      return $location.path('/');
+
+    $scope.settings = {};
 
     $scope.forget = function (dev) {
-      trezorService.forget(dev.serialNumber);
+      trezorService.forget(dev.id);
       $location.path('/');
+    };
+
+    $scope.setup = function (dev, settings) {
+      if (settings.label)
+        settings.label = settings.label.trim();
+
+      dev.reset(settings)
+        .then(
+          function (result) { $location.path('/'); },
+          function (err) { alert(err); }
+        );
+    };
+
+    $scope.loadByXprv = function (dev, settings) {
+      if (settings.label)
+        settings.label = settings.label.trim();
+      if (settings.xprv)
+        settings.xprv = settings.xprv.trim();
+
+      dev.loadByXprv(settings)
+        .then(
+          function (result) { $location.path('/'); },
+          function (err) { alert(err); }
+        );
     };
   });
